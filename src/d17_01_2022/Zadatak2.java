8741c8c1d0c940dd4704202677ae37d531b50006
Saska Jovanovic <sashkasta@gmail.com>
2022-01-19 22:09:41 +0100

    Domaci 17_01_2022 potencijalna izmena

--- src/d17_01_2022/Zadatak2.java
+++ src/d17_01_2022/Zadatak2.java
@@ -9,18 +9,18 @@ public class Zadatak2 {
 		Racun racun1 = new Racun("4646464646", "Pera", "Peric", 10000);
 		Racun racun2 = new Racun("2121212121", "Mika", "Mikic", 0);
 		
-//		Transakcija transakcija1 = new Transakcija(1, racun1, racun2);
-//		double vrednostTransakcije = 1000;
-//		transakcija1.izvrsiTransakciju(vrednostTransakcije);
-//		transakcija1.stampaj();
-//		System.out.println();
-//		Transakcija transakcija2 = new Transakcija(1, racun1, racun2);
-//		vrednostTransakcije = 5000;
-//		transakcija2.izvrsiTransakciju(vrednostTransakcije);
-//		transakcija2.stampaj();
-//		System.out.println();
-		Transakcija transakcija3 = new Transakcija(1, racun2, racun1);
+		Transakcija transakcija1 = new Transakcija(1, racun1, racun2);
 		double vrednostTransakcije = 1000;
+		transakcija1.izvrsiTransakciju(vrednostTransakcije);
+		transakcija1.stampaj();
+		System.out.println();
+		Transakcija transakcija2 = new Transakcija(1, racun1, racun2);
+		vrednostTransakcije = 5000;
+		transakcija2.izvrsiTransakciju(vrednostTransakcije);
+		transakcija2.stampaj();
+		System.out.println();
+		Transakcija transakcija3 = new Transakcija(1, racun2, racun1);
+		vrednostTransakcije = 1000;
 		transakcija3.izvrsiTransakciju(vrednostTransakcije);
 		transakcija3.stampaj();
 
